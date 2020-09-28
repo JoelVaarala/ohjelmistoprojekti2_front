a1bbda610b63011e536bc6b630945773c3a1096d
Jaani Savolainen <jaani.savolainen@gmail.com>
2020-09-28 18:20:06 +0300

    lisätty käyttäjän oman tiedon haku firebasesta

--- components/EditProfile.js
+++ components/EditProfile.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Icon, Input } from 'react-native-elements'
+import auth from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
 
 
 //Käyttäjän tagit, bio ja kuvat. Nimeä ja ikää ei voi vaihtaa
 export default function EditProfile() {
 
+
+  React.useEffect(() => {
+    HaeKayttajanTiedot()
+  }, []);  
+
+  const HaeKayttajanTiedot = async () => {
+  
+    console.log("Haetaan käyttäjän omat tiedot")
+    const myUID = auth().currentUser.uid;
+    const tiedot = await firestore().collection("users").doc(myUID).get();
+    console.log(tiedot._data)
+    //Nyt tiedot kentästä voi noukkia tarvittavat tiedot.
+  }
 
   return (
     <View style={styles.container}>
