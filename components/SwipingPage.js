d65c5c93da97e6fed8c4d5367b62cad077ef447c
Jussi S <bgh284@myy.haaga-helia.fi>
2020-10-15 10:13:37 +0300

    buttongroupin väriä muutettu kikkailemalla

--- components/SwipingPage.js
+++ components/SwipingPage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
-import { Icon, ButtonGroup } from "react-native-elements";
+import { Icon, ButtonGroup, ThemeProvider } from "react-native-elements";
 import SwipeCards from "./SwipeCards";
 import * as Location from 'expo-location';
 
@@ -79,25 +79,35 @@ export default function SwipingPage({ navigation, route }) {
   const [selectedIndex, setSelectedIndex] = React.useState({ main: 2, sub: [1] });
 
 
+  const theme = {
+    colors: {
+      primary: 'orange'
+    }
+    
+  }
+
   return (
     <View style={styles.container}>
+      <ThemeProvider theme={theme}>
 
         <ButtonGroup
           onPress={value => updateIndex('main', value)}
           selectedIndex={selectedIndex.main}
           buttons={buttons}
           containerStyle={{ height: 40 }}
+
         />
         {selectedIndex.main != 0 ? (
           <ButtonGroup
-          onPress={value => updateIndex('sub', value)}
-          selectMultiple={true}
-          selectedIndexes={selectedIndex.sub}
-          buttons={subButtons}
-          containerStyle={{ height: 40 }}
-          style={{paddingBottom: 50}}
-        />
+            onPress={value => updateIndex('sub', value)}
+            selectMultiple={true}
+            selectedIndexes={selectedIndex.sub}
+            buttons={subButtons}
+            containerStyle={{ height: 40 }}
+            style={{ paddingBottom: 50 }}
+          />
         ) : (null)}
+      </ThemeProvider>
 
       <View style={{ flex: 1, justifyContent: "flex-start", paddingTop: 50 }}>
         {/* <SwipeCards vaihtoehdot={swipettavat} /> */}
