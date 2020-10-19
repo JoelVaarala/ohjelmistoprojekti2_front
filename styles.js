02629784bed823503f6606a1ff276d43ea8c04d4
chuaaang <bgf915@myy.haaga-helia.fi>
2020-10-19 19:47:23 +0300

    käyttämättömien tyylien poisto

--- styles.js
+++ styles.js
@@ -1,193 +1,133 @@
 import { StyleSheet } from 'react-native';
 
-
 export default StyleSheet.create({
 
-      container: {
+    //Containers
+    container: {
         flex: 1,
         alignItems: 'flex-start',
         justifyContent: 'flex-start',
       },
-
       containerFlexStart: {
         flex: 1,
         justifyContent: 'flex-start'
       },
-
       containerAlignItemsCenter: {
         flex: 1,
         alignItems: 'center'
       },
-
-      alignSelfCenter: {
-        alignSelf: 'center'
-      },
-
-      flexThree: {
-        flex: 3
-      },
-
-      marginLeftTwenty: {
-        marginLeft: 20,
-      },
-
-      heightForty: {
-        height: 40
-      },
-
       containerCenter: {
         alignItems: "center",
         justifyContent: "center",
       },
-
-      backgroundBlack: {
-        backgroundColor: 'black'
+      cardContainer: { 
+        position: "absolute", 
+        top: -75, 
+        left: 15, 
+        right: 150, 
+        justifyContent: "flex-end", 
+        alignItems: "flex-start" 
       },
-
-      marginTopFifty: {
-        marginTop: 50
-      },
-
-      marginTopThirty: {
-        marginTop: 30
-      },
-
-      paddingTopTwenty: {
-        paddingTop: 50
-      },
-
-      paddingTopFifty: {
-        paddingTop: 50,
-      },
-
-      paddingTopTen: {
-        paddingTop: 10,
-      },
-
-      marginLeftTen: {
-        marginLeft: 10
-      },
-
-      marginTopTen: {
-        marginTop: 10
-      },
-
-      paddingBottomFifty: {
-        paddingBottom: 50
-      },
-
-      paddingTopHundred: {
-        paddingTop: 100
-      },
-
-      iconsPadding: {
-        paddingLeft: 20,
-        paddingRight: 20,
-        paddingBottom: 20,
-      },
-
-      flexDirectionRow: {
-        flexDirection: 'row'
-      },
-
-      justifyContentSpaceBetween: {
-        justifyContent: 'space-between'
-      },
-
-      alignItemsFlexEnd: {
-        alignItems: 'flex-end'
-      },
-
-      textOrangeBold: {
-        color: 'orange',
-        fontWeight: 'bold'
-      },
-
-      orangeMarginTopFive: {
-        color: 'orange',
-        marginTop: 5
-      },
-
-      saveButton: {flex: 1, 
-        marginLeft: 80, 
-        marginRight: 80
-      },
-
-      addTagInputBox: {
-        height: 40, 
-        width: 200, 
-        backgroundColor: 'white', 
-        color: 'black'
-      },
-
-      userTextStyle: {
-        fontSize: 40,
-        color: 'orange'
-      },
-
-      userBioStyle: {
-        fontSize: 20,
-        color: 'orange'
-      },
-
-      fontSizeTwenty: {
-        fontSize: 20
-      },
-
-      viewLikersContainer: {
-        paddingTop: 50,
+      carouselContainer: {
         flex: 1,
-        backgroundColor: '#fff',
-      },
-
-      startingscreenContainer: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
+        flexDirection: 'row',
         justifyContent: 'center',
+    },
+      myProfileAvatarContainer: {
+        marginTop: 15,
+        alignItems: 'center'
       },
 
-      editProfileTextAreaContainer: { 
-        height: 90, 
-        width: 500, 
-        backgroundColor: 'white', 
-        color: 'black' 
-      },
-
-      myProfileContainer: {
-        flex: 1,
-        backgroundColor: '#fff'
-      },
-
-      profileContainer: {
-        alignItems: 'center',
-        flex: 6,
-      },
-
-      settingsContainer: {
-        flex: 1,
-        backgroundColor: '#fff',
-      },
-
-      button: {
-        alignItems: "center",
-        backgroundColor: "#DDDDDD",
-        padding: 10
-      },
-      headers: {
-        fontWeight: 'bold',
-        marginTop: 10
-      },
-      textbox: {
-        height: 40,
-        width: 200,
-        borderBottomColor: 'gray',
-        borderBottomWidth: 1,
-      },
+      //flex
 
       flexOne: {
         flex: 1,
       },
+      flexThree: {
+        flex: 3
+      },
+
+    //fontFamily
+    textGreyRoboto: {
+      color: 'grey',
+      fontFamily: 'roboto'
+    },
+
+    fontRoboto: {
+      fontFamily: 'roboto'
+    },
+
+
+
+    //flexDirection
+
+    flexDirectionRow: {
+      flexDirection: 'row'
+    },
+
+      //Chat
+
+      chatStyle: { 
+        justifyContent: 'space-around', 
+        flexDirection: 'row', 
+        padding: 5, 
+        backgroundColor: 'black' 
+    },
+
+      //viewLikersItemContent
+      viewLikersItemContent: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+
+      //title
+      registerTitleStyle: {
+        color: 'grey',
+        fontSize: 17,
+        fontWeight: 'bold',
+        marginBottom: 10
+    },
+
+    //carousel
+
+    carouselSafeAreaView: {
+        flex: 1,
+        paddingTop: 0,
+    },
+
+
+
+      //no more cards
+
+      noMoreCardsContent: {
+        flex: 1,
+        justifyContent: 'center'
+      },
+
+      //image styling
+
+      //Card bottom shadow thingy
+      darkishStyle: { 
+        height: 400, 
+        width: 350, 
+        bottom: -100, 
+        right: 15, 
+        opacity: 0.9, 
+        borderRadius: 5 
+      },
+
+      matchesImage: {
+        flex: 1,
+        resizeMode: "cover",
+        justifyContent: "center"
+      },
+
+      imageSize: {
+        width: '100%', 
+        height: '100%'
+      },
+
+      //buttons
 
       previewButtonStyle: {
         flexDirection: 'row', 
@@ -195,35 +135,191 @@ export default StyleSheet.create({
         alignItems: 'center', 
         justifyContent: 'space-evenly'
       },
+      registerUserButton: {
+        paddingHorizontal: 10,
+        paddingTop: 10,
+        paddingBottom: 80,
+        },
+        saveButton: {
+            flex: 1, 
+            marginLeft: 80, 
+            marginRight: 80
+        },
+        viewLikersbutton: {
+            alignItems: "center",
+            backgroundColor: "#DDDDDD",
+            padding: 10
+        },
+        button: {
+            alignItems: "center",
+            backgroundColor: "#DDDDDD",
+            padding: 10
+          },
 
+      //backgroundcolors
+
+      backgroundBlack: {
+        backgroundColor: 'black'
+      },
+      carouselBackgroundFloralWhite: {
+        backgroundColor: 'floralwhite',
+        flex: 1,
+      },
       matchesBackgroundColor: {
         backgroundColor: 'rgba(255,154,0,0)' 
       },
 
-      fontRoboto: {
-        fontFamily: 'roboto'
+      //height & width
+      heightForty: {
+        height: 40
+      },
+      viewLikersPickerSize: {
+        height: 50,
+        width: 150,
+    },
+      //Space between icons
+      iconSpacing: {
+        flex: 1, 
+        flexDirection: 'row', 
+        justifyContent: 'space-around', 
+        top: 20
       },
 
-      textGreyRoboto: {
-        color: 'grey',
-        fontFamily: 'roboto'
+      //alignSelfCenter
+
+      alignSelfCenter: {
+        alignSelf: 'center'
       },
 
-      opacityOne: {
-        opacity: 1
+      //scrollView
+    addEventScrollView: {
+        flex:1,
+        marginTop: 22,
+    },
+
+    //alignItems
+
+    alignItemsFlexEnd: {
+        alignItems: 'flex-end'
       },
-//___________________________________________________________________________________________________
-      //SwipeCard-components
+
+      //justifyContent
+
+      justifyContentSpaceBetween: {
+        justifyContent: 'space-between'
+      },
+
+    //fontSize
+    userTextStyle: {
+        fontSize: 40,
+        color: 'orange'
+      },
+      userBioStyle: {
+        fontSize: 20,
+        color: 'orange'
+      },
+      fontSizeTwenty: {
+        fontSize: 20
+      },
       noMoreCardsText: {
         fontSize: 22,
       },
-
-      //No more cards content at center of screen
-      noMoreCardsContent: {
-        flex: 1,
-        justifyContent: 'center'
+      eventInfo: { 
+        fontSize: 18, 
+        color: "white", 
+        fontWeight: "bold" 
+    },
+    eventInfoBio: { 
+        fontSize: 12,
+        color: "white" 
+    },
+    editProfileText: {
+        fontSize: 20,
+        paddingTop: 2,
+        paddingBottom: 1,
+      },
+      textOrangeBold: {
+        color: 'orange',
+        fontWeight: 'bold'
       },
 
+    //tagBox
+
+    tagBox: {
+        padding: 6,
+        fontSize: 16,
+        color: 'black',
+        marginVertical: 7,
+        marginHorizontal: 10,
+        backgroundColor: 'orange',
+        borderRadius: 6,
+      },
+
+      //opacity
+      opacityOne: {
+        opacity: 1,
+      },
+
+      //textbox things
+      eventTextbox: {
+        height: 40,
+        width: 200,
+        borderBottomColor: 'gray',
+        borderBottomWidth: 1,
+      },
+
+      //textInput
+      tagTextInput: { 
+        height: 40, 
+        width: 200, 
+        backgroundColor: 'white', 
+        color: 'black' 
+      },
+      addTagInputBox: {
+        height: 40, 
+        width: 200, 
+        backgroundColor: 'white', 
+        color: 'black'
+      },
+
+      //textArea
+
+      editProfileTextArea: {
+        textAlignVertical: "top",
+        alignSelf: 'stretch',
+        fontSize: 15
+      },
+      editProfileTextAreaContainer: { 
+        height: 90, 
+        width: 500, 
+        backgroundColor: 'white', 
+        color: 'black' 
+      },
+
+      //radioInput
+        /* 
+    registerRadioGroup: {
+        width: 22,
+        height: 22,
+        borderColor: '#000',
+        borderWidth: 0.8,
+        marginRight: 10,
+        fillColor: '#279315'
+    },
+    */
+
+    //rangerSlider
+    settingsRangerSlider: {
+        width: 250,
+        height: 60,
+    },
+
+    //paddingHorizontal
+    paddingHorizontalTen: {
+        paddingHorizontal: 10,
+    },
+
+      //card things
       card: {
         justifyContent: "center",
         alignItems: "center",
@@ -236,376 +332,63 @@ export default StyleSheet.create({
         bottom: 20,
       },
 
-      //Card bottom shadow thingy
-      darkishStyle: { 
-        height: 400, 
-        width: 350, 
-        bottom: -100, 
-        right: 15, 
-        opacity: 0.9, 
-        borderRadius: 5 
+      //logo
+      matchesLogo: {
+        // justifyContent: 'center',
+        // alignContent: 'center',
+        // alignItems: 'center',
+        width: 400,
+        height: 50,
+        padding: '15%'
       },
-      
-      //Card size
-      cardContainer: { 
-      position: "absolute", 
-      top: -75, 
-      left: 15, 
-      right: 150, 
-      justifyContent: "flex-end", 
-      alignItems: "flex-start" 
+
+
+      //margin
+
+      eventAddMargin: {
+        marginLeft: 10,
+        marginRight: 10
     },
 
-    //Event info at bottom side of card
-    marginBottomForty: {
+      marginLeftTen: {
+        marginLeft: 10
+      },
+      marginLeftTwenty: {
+        marginLeft: 20,
+      },
+      marginTopTen: {
+        marginTop: 10
+      },
+      marginTopThirty: {
+        marginTop: 30
+      },
+      marginTopFifty: {
+        marginTop: 50
+      },
+      marginBottomForty: {
         marginBottom: 40
     },
-
-    //Event info text style
-    eventInfo: { 
-        fontSize: 18, 
-        color: "white", 
-        fontWeight: "bold" 
-    },
-
-    //Event info bio style
-    eventInfoBio: { 
-        fontSize: 12,
-        color: "white" 
-    },
-
-//___________________________________________________________________________________________________
-//SwipingPage
-  
-  //Icons location at bottom of the screen
-  iconLocation: {
-    flex: 1, 
-    justifyContent: "flex-start"
-  },
-
-  //Space between icons
-  iconSpacebetween: {
-    flex: 1, flexDirection: "row", 
-    justifyContent: "space-between", 
-    alignItems: "flex-end"
-  },
-
-  //Padding between icons
-  iconPadding: {
-    paddingLeft: 20,
-    paddingRight: 20,
-    paddingBottom: 20,
-  },
-
-  //Mikä buttoni tää on?!?
-  button: {
-    alignItems: "center",
-    backgroundColor: "#DDDDDD",
-    padding: 10,
-  },
-
-//___________________________________________________________________________________________________
-//Add_Event
-
-addEventScrollView: {
-    flex:1,
-    marginTop: 22,
-},
-
-eventAddMargin: {
-    marginLeft: 10,
-    marginRight: 10
-},
-  /*
-  ei käytössä?
-  button: {
-    alignItems: "center",
-    backgroundColor: "#DDDDDD",
-    padding: 10
-  },
-  */
-  eventHeaders: {
-    fontWeight: 'bold',
-    marginTop: 10
-  },
-  eventTextbox: {
-    height: 40,
-    width: 200,
-    borderBottomColor: 'gray',
-    borderBottomWidth: 1,
-  },
-
-//___________________________________________________________________________________________________
-//Carousel
-
-carouselSafeAreaView: {
-    flex: 1,
-    paddingTop: 0,
-},
-
-carouselContainer: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'center',
-},
-
-imageSize: {
-  width: '100%', 
-  height: '100%'
-},
-
-carouselBackgroundFloralWhite: {
-  backgroundColor: 'floralwhite',
-  flex: 1,
-},
-
-//___________________________________________________________________________________________________
-//Chat
-
-
-
-chatStyle: { 
-    justifyContent: 'space-around', 
-    flexDirection: 'row', 
-    padding: 5, 
-    backgroundColor: 'black' 
-},
-
-
-//___________________________________________________________________________________________________
-//EditProfile
-
-  editProfileTextArea: {
-    textAlignVertical: "top",
-    alignSelf: 'stretch',
-    fontSize: 15
-  },
-  editProfileText: {
-    fontSize: 20,
-    paddingTop: 2,
-    paddingBottom: 1,
-  },
-
-  omatContainerit: {
-    flex: 4,
-    paddingTop: 20,
-    alignItems: 'flex-start',
-    paddingLeft: 80
-  },
-
-  tagTextInput: { 
-    height: 40, 
-    width: 200, 
-    backgroundColor: 'white', 
-    color: 'black' 
-  },
-
-//___________________________________________________________________________________________________
-//Globaalit
-
-
-
-//ei käytössä?
-matchesButton: {
-    alignItems: "center",
-    backgroundColor: "#DDDDDD",
-    padding: 10
-  },
-  matchesImage: {
-    flex: 1,
-    resizeMode: "cover",
-    justifyContent: "center"
-  },
-  matchesLogo: {
-    // justifyContent: 'center',
-    // alignContent: 'center',
-    // alignItems: 'center',
-    width: 400,
-    height: 50,
-    padding: '15%'
-  },
-
-  matchMessage: {
-    fontSize: 20, color: 'orange',
-    fontWeight: 'bold',
-    fontFamily: 'roboto'
-  },
-
-  matchContainerBackground: {
-    backgroundColor: 'rgba(255,154,0,0)'
-  },
-  
-  opacityOne: {
-    opacity: 1,
-  },
-
-  listItemTitleStyle: {
-    color: 'white', 
-    fontWeight: 'bold',
-    fontFamily: 'roboto'
-  },
-
-  listItemSubtitleStyle: {
-    color: 'gray',
-    fontFamily: 'roboto'
-  },
-
-  matchesTextStyle: {
-    fontSize: 20, 
-    color: 'orange',
-    fontWeight: 'bold',
-    fontFamily: 'roboto'
-  },
-
-  avatarRoundedBackground: {
-    backgroundColor: 'rgba(255,154,0,0)'
-  },
-
-//___________________________________________________________________________________________________
-//MyProfile
-
-  myProfileAvatarContainer: {
-    marginTop: 15,
-    alignItems: 'center'
-  },
-
-  iconSpacing: {
-    flex: 1, 
-    flexDirection: 'row', 
-    justifyContent: 'space-around', 
-    top: 20
-  },
-
-
-//___________________________________________________________________________________________________
-//Register
-
-registerScrollView: {
-    flex: 1,
-    paddingTop: 20,
-},
-
-registerText: {
-    alignSelf: 'center'
-},
-
-registerBirthdayMarginLeft: {
-    marginLeft: '10'
-},
-
-registerTitleStyle: {
-    color: 'grey',
-    fontSize: 17,
-    fontWeight: 'bold',
-    marginBottom: 10
-},
-
-registerDatePicker: {
-    alignSelf: 'center',
-},
-
-registerGenderStyle: {
-    marginLeft: 10,
-    paddingTop: 10,
-},
-
-registerGenderText: {
-    color: 'grey', 
-    fontSize: 17,
-    fontWeight: 'bold',
-    marginBottom: 10,
-},
-
-registerUserButton: {
-    paddingHorizontal: 10,
-    paddingTop: 10,
-    paddingBottom: 80,
-},
-
-heightForty: {
-  height: 40
-},
-
-/* 
-registerRadioGroup: {
-      width: 22,
-      height: 22,
-      borderColor: '#000',
-      borderWidth: 0.8,
-      marginRight: 10,
-      fillColor: '#279315'
-},
-*/
-
-//___________________________________________________________________________________________________
-//Settings
-  tagBox: {
-    padding: 6,
-    fontSize: 16,
-    color: 'black',
-    marginVertical: 7,
-    marginHorizontal: 10,
-    backgroundColor: 'orange',
-    borderRadius: 6,
-  },
-
-  settingsRangerSlider: {
-      width: 250,
-      height: 60,
-  },
-
-//___________________________________________________________________________________________________
-//Startingscreen
-
-
-
-//___________________________________________________________________________________________________
-//Startup
-
-StartupUsername: {
-    paddingTop: 100,
-},
-
-paddingHorizontalTen: {
-    paddingHorizontal: 10,
-},
-
-//___________________________________________________________________________________________________
-//Startup
-
-
-viewLikersbutton: {
-    alignItems: "center",
-    backgroundColor: "#DDDDDD",
-    padding: 10
-},
-viewLikersPicker: {
-    //flex: 1,
-    // paddingTop: 0,
-    alignItems: "center"
-},
-
-viewLikersPickerSize: {
-    height: 50,
-    width: 150,
-},
-
-viewLikersSwipedFontSize: {
-    fontSize: 20,
-},
-
-viewLikersItemContent: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-},
-
-viewLikersButton: {
-  alignItems: "center",
-  backgroundColor: "#DDDDDD",
-  padding: 10
-},
-
+    orangeMarginTopFive: {
+        color: 'orange',
+        marginTop: 5
+      },
+
+    //padding
+    paddingTopTen: {
+        paddingTop: 10,
+      },
+      paddingTopFifty: {
+        paddingTop: 50,
+      },
+      paddingTopHundred: {
+        paddingTop: 100
+      },
+      paddingBottomFifty: {
+        paddingBottom: 50
+      },
+      iconsPadding: {
+        paddingLeft: 20,
+        paddingRight: 20,
+        paddingBottom: 20,
+      },
 });
-
-
-
