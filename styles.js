adcf162fd6174d6e51444fe394aa9a3abd7dae7c
JoelVaarala <bgh275@myy.haaga-helia.fi>
2020-11-08 15:06:18 +0200

    map fix

--- styles.js
+++ styles.js
@@ -446,16 +446,14 @@ registerRadioGroup: {
   },
   // Event modal
   mapView: {
+    flex: 1,
     position: "absolute",
-    borderRadius: 20,
-    borderWidth: 2,
-    margin: 20,
     top: 0,
-    left: 8,
+    left: 0,
     right: 0,
     bottom: 0,
-    width: "110%",
-    height: "80%",
+    width: '100%',
+    height: '100%',
   },
   viewFirst: { flex: 1, justifyContent: "center", alignItems: "center", marginTop: 22 },
   viewSecond: {
